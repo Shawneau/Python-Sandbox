10ff3f3c93121319d9e8063542117c1d3f094e8b
Shawn Oliver <olivershawn@gmail.com>
2020-01-02 22:42:04 -0500

    Adding New

---
+++ New.py
@@ -0,0 +1,12 @@
+print ('This is a test')
+print ('fffffuuuu')
+testVariable = input()
+print (testVariable + ' is a weird thing to say')
+print ("Let's continue")
+print ('What is your name')
+userName = input()
+print ('Hello ' + userName)
+
+
+
+
